1e512a100c65f24de427b67299e1b51d7f160aa3
user <A00278807@mycambrian.ca>
2023-10-14 20:57:55 -0400

    created functions

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -36,5 +39,31 @@ fun GreetingView(text: String) {
 fun DefaultPreview() {
     MyApplicationTheme {
         GreetingView("Hello, Android!")
+    }
+}
+
+@Composable
+fun CalcView() {
+
+    }
+@Composable
+fun CalcRow() {
+
+}
+
+@Composable
+fun CalcDisplay() {
+
+}
+
+@Composable
+fun CalcOperationButton() {
+
+}
+
+@Composable
+fun CalcEqualsButton(display: MutableState<String>) {
+    Button(onClick = { /*TODO*/ }, Modifier.padding(4.dp)) {
+        
     }
 }
