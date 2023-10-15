f25281b0cac6fe2f67f790ce228ff9c4782f0ee4
user <A00278807@mycambrian.ca>
2023-10-14 21:19:09 -0400

    calcView

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -4,11 +4,15 @@ import android.media.VolumeShaper.Operation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,16 +49,25 @@ fun DefaultPreview() {
 
 @Composable
 fun CalcView() {
-
-    }
+val displayText = {mutableStateOf("0")}
+}
 @Composable
-fun CalcRow() {
-
+fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
+ val endNum = startNum + numButtons
+    Row(Modifier.padding(0.dp)){
+        for (i in startNum downTo endNum) {
+            CalcNumericButton(number = i, display = display)
+        }
+    }
 }
 
 @Composable
-fun CalcDisplay() {
-
+fun CalcDisplay(display: MutableState<String>) {
+Text(text = display.value,
+    Modifier
+        .height(50.dp)
+        .padding(5.dp)
+        .fillMaxWidth())
 }
 
 @Composable
@@ -67,8 +80,8 @@ fun CalcOperationButton(operation: String, display: MutableState<String>) {
 
 @Composable
 fun CalcNumericButton(number: Int, display: MutableState<String>) {
-    Button(onClick = { display.value = "0" }, Modifier.padding(4.dp)) {
-        Text(text = "=")
+    Button(onClick = {display.value += number.toString()}, Modifier.padding(4.dp)) {
+        Text(text = number.toString())
     }
 }
 
