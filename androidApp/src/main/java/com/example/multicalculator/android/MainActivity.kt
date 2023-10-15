150688869e5392226968ef29047aed9b11ea1b63
user <A00278807@mycambrian.ca>
2023-10-14 21:25:01 -0400

    CalcDisplay call

--- androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multicalculator/android/MainActivity.kt
@@ -4,6 +4,8 @@ import android.media.VolumeShaper.Operation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,7 +15,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.multicalculator.Greeting
@@ -49,7 +53,20 @@ fun DefaultPreview() {
 
 @Composable
 fun CalcView() {
-val displayText = {mutableStateOf("0")}
+val displayText = remember {mutableStateOf("0")}
+    Column(Modifier.background(color = Color.LightGray)){
+        Row(){
+            CalcDisplay(displayText)
+        }
+        Row(){
+            Column(){
+
+            }
+            Column(){
+
+            }
+        }
+    }
 }
 @Composable
 fun CalcRow(display: MutableState<String>, startNum: Int, numButtons: Int) {
